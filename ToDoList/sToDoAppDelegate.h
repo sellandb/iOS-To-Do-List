1c123e17cac273f0a42f212188caa7405b86aa65
sellandb <sellandb@gmail.com>
2014-02-23 00:59:49 -0500

    Initial Commit

---
+++ ToDoList/sToDoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  sToDoAppDelegate.h
+//  ToDoList
+//
+//  Created by Selland, Ben on 2/23/14.
+//  Copyright (c) 2014 Selland, Ben. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sToDoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
